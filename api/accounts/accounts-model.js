e6e3b5a571580ca020a156e3a4b71bde8ded56b3
ClintFix <clint.fix@gmail.com>
2021-04-12 13:31:04 -0600

    get all accounts router and model added

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,5 +1,8 @@
+//import db
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db("budget")
 }
 
 const getById = id => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,8 +1,15 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model')
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  Accounts.getAll()
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(err => {
+      next(err);
+    })
+});
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
@@ -20,8 +27,8 @@ router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
 })
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+router.use((err, _, res, ) => {
+  res.status(500).json({ message: err.message, stack: err.stack })
 })
 
 module.exports = router;
